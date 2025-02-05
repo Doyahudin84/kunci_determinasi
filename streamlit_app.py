3c17d8e9f9d01eac0235ca733f03187a35793dc3
Doyahudin <103917673+Doyahudin84@users.noreply.github.com>
2025-02-05 17:19:52 +0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit as st
 col1, col2, col3 = st.columns([1, 3, 1])
 # Judul Aplikasi
 with col2:
-    st.image('aranademi.png', width=200)
-st.title("Kunci Determinasi Jenis Ikan")
+    st.image('aranademi.png', width=300)
+st.title("Kunci Determinasi Tumbuhan")
 
 # Deskripsi aplikasi
 st.write(
@@ -13,51 +13,32 @@ st.write(
     """
 )
 
-# Fungsi untuk menentukan jenis ikan berdasarkan input
-def identifikasi_ikan(sirip_punggung, tubuh, sirip_panjang, mulut_besar, habitat):
-    if sirip_punggung == "Ganda":
-        return "Ikan Kerapu"
-    elif tubuh == "Ramping dan panjang":
-        if sirip_panjang == "Panjang dan torpedo":
-            return "Ikan Tuna"
-        else:
-            return "Ikan Tenggiri"
-    elif mulut_besar == "Ya":
-        return "Ikan Hiu"
-    elif habitat == "Dasar perairan atau terumbu karang":
-        return "Ikan Kakap"
-    else:
-        return "Ikan Nila"
+# Langkah pertama
+st.subheader("1. Apakah tumbuhan memiliki klorofil dan dapat fotosintesis?")
+tumbuhan_klorofil = st.radio("Pilih salah satu:", ('Tidak memiliki klorofil dan tidak dapat fotosintesis', 'Memiliki klorofil dan dapat fotosintesis'))
 
-# Input pengguna
-sirip_punggung = st.selectbox(
-    "Apakah ikan memiliki sirip punggung ganda atau tunggal?",
-    ["Tunggal", "Ganda"]
-)
+if tumbuhan_klorofil == 'Tidak memiliki klorofil dan tidak dapat fotosintesis':
+    st.write("Identifikasi: **Jamur**")
+elif tumbuhan_klorofil == 'Memiliki klorofil dan dapat fotosintesis':
+    # Langkah kedua
+    st.subheader("2. Apakah tumbuhan tersebut menghasilkan biji?")
+    menghasilkan_biji = st.radio("Pilih salah satu:", ('Tidak menghasilkan biji', 'Menghasilkan biji'))
 
-tubuh = st.selectbox(
-    "Apakah ikan memiliki tubuh ramping dan panjang?",
-    ["Ramping dan panjang", "Gemuk atau bulat"]
-)
+    if menghasilkan_biji == 'Tidak menghasilkan biji':
+        st.write("Identifikasi: **Lumut**")
+    elif menghasilkan_biji == 'Menghasilkan biji':
+        # Langkah ketiga
+        st.subheader("3. Apakah biji tumbuhan terbuka atau tertutup?")
+        biji_terbuka_tertutup = st.radio("Pilih salah satu:", ('Biji terbuka, tidak memiliki bunga, sering berbentuk konus atau kerucut', 'Biji tertutup dalam buah'))
 
-sirip_panjang = st.selectbox(
-    "Apakah sirip punggung ikan panjang dan tubuhnya seperti torpedo?",
-    ["Panjang dan torpedo", "Tidak"]
-)
+        if biji_terbuka_tertutup == 'Biji terbuka, tidak memiliki bunga, sering berbentuk konus atau kerucut':
+            st.write("Identifikasi: **Gymnospermae**")
+        elif biji_terbuka_tertutup == 'Biji tertutup dalam buah':
+            # Langkah keempat
+            st.subheader("4. Apakah tumbuhan memiliki daun dengan satu kotiledon atau dua kotiledon?")
+            kotiledon = st.radio("Pilih salah satu:", ('Daun dengan urat sejajar, memiliki satu kotiledon', 'Daun dengan urat bersirip, memiliki dua kotiledon'))
 
-mulut_besar = st.selectbox(
-    "Apakah ikan memiliki mulut besar dengan gigi tajam?",
-    ["Ya", "Tidak"]
-)
-
-habitat = st.selectbox(
-    "Apakah ikan sering ditemukan di dasar perairan atau terumbu karang?",
-    ["Dasar perairan atau terumbu karang", "Perairan terbuka"]
-)
-
-# Tombol untuk menampilkan hasil
-if st.button("Identifikasi Jenis Ikan"):
-    hasil = identifikasi_ikan(sirip_punggung, tubuh, sirip_panjang, mulut_besar, habitat)
-    st.write(f"Jenis ikan yang Anda identifikasi adalah: **{hasil}**")
-
-
+            if kotiledon == 'Daun dengan urat sejajar, memiliki satu kotiledon':
+                st.write("Identifikasi: **Monokotil**")
+            elif kotiledon == 'Daun dengan urat bersirip, memiliki dua kotiledon':
+                st.write("Identifikasi: **Dikotil**")
